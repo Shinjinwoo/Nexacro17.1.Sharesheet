0362317c198d863a985f9c48097401e2d0134ed8
tobesoft <jinu712@gmail.com>
2022-08-24 16:19:31 +0900

    rxJava를 활용한 ShareSheet 모듈 오브젝트 미생성시 비동기 처리

--- app/src/main/java/com/example/androidruntime/NexacroActivityExt.java
+++ app/src/main/java/com/example/androidruntime/NexacroActivityExt.java
@@ -1,28 +1,22 @@
 package com.example.androidruntime;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.JsonWriter;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import com.nexacro.NexacroActivity;
-import com.tobesoft.plugin.sharesheet.DataBidingViewModel;
 import com.tobesoft.plugin.sharesheet.PreferenceManager;
 import com.tobesoft.plugin.sharesheet.ShareSheetObject;
 import com.tobesoft.plugin.sharesheet.plugininterface.ShareSheetInterface;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.reactivestreams.Subscriber;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableEmitter;
-import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.observers.DisposableObserver;
 
 
 public class NexacroActivityExt extends NexacroActivity implements ShareSheetInterface {
@@ -43,20 +37,29 @@ public class NexacroActivityExt extends NexacroActivity implements ShareSheetInt
     @Override
     protected void onResume() {
         super.onResume();
-        String sendText = PreferenceManager.getString(getApplicationContext(), "testKey");
-        if (!sendText.equals("")) {
-            try {
-                JSONObject jsonObject = new JSONObject(sendText);
-
-                if(mShareSheetObject != null ) {
-                    mShareSheetObject.execute(jsonObject);
-                    Log.e(LOG_TAG, "::::::::::::::::::::::::::" + sendText);
-                    Log.e(LOG_TAG, "onResume");
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
+//        String sendText = PreferenceManager.getString(getApplicationContext(), "testKey");
+//        if (!sendText.equals("")) {
+//            try {
+//                JSONObject jsonObject = new JSONObject(sendText);
+//
+//                if( mShareSheetObject != null ) {
+//                    mShareSheetObject.execute(jsonObject);
+//                    Log.e(LOG_TAG, "::::::::::::::::::::::::::" + sendText);
+//                    Log.e(LOG_TAG, "onResume");
+//                } else {
+//                    Handler handler = new Handler(Looper.getMainLooper());
+//                    handler.postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            mShareSheetObject.execute(jsonObject);
+//                        }
+//                    },3000);
+//                    Log.e(LOG_TAG,"mShareSheetObject is null");
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     @Override
@@ -81,7 +84,32 @@ public class NexacroActivityExt extends NexacroActivity implements ShareSheetInt
     @Override
     public void setShareSheetObject(ShareSheetObject obj) {
         this.mShareSheetObject = obj;
+        Observable<ShareSheetObject> source = Observable.create(emitter -> {
+            emitter.onNext(mShareSheetObject);
+            emitter.onComplete();
+        });
+
+        source.subscribe(executeShareSheetObject(mShareSheetObject));
     }
+
+    public @NonNull Observer<? super ShareSheetObject> executeShareSheetObject(ShareSheetObject shareSheetObject) {
+        String sendText = PreferenceManager.getString(getApplicationContext(), "testKey");
+        if (!sendText.equals("")) {
+            try {
+                JSONObject jsonObject = new JSONObject(sendText);
+
+                if( shareSheetObject != null ) {
+                    shareSheetObject.execute(jsonObject);
+                    Log.e(LOG_TAG, "::::::::::::::::::::::::::" + sendText);
+                    Log.e(LOG_TAG, "onResume");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 
     /** Sharesheet 연동 코드 ****************************************************************************/
 
